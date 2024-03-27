5a81c1f0504efd36e99c46f248948142a6134ae6
Caio Proença Gomes <caio.proenca@pucpr.edu.br>
2024-03-27 19:47:43 -0300

    MobileAppApplication:useChuckApi

--- AndroidWebFirstApplication/app/src/main/java/com/example/androidfirstapplication/MainActivity.kt
+++ AndroidWebFirstApplication/app/src/main/java/com/example/androidfirstapplication/MainActivity.kt
@@ -2,9 +2,17 @@ package com.example.androidfirstapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil.*
+import androidx.databinding.ViewDataBinding
+import com.example.androidfirstapplication.databinding.ActivityMainBinding
 import com.google.gson.Gson
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import androidx.databinding.DataBindingUtil as DataBindingUtil1
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,10 +22,18 @@ class MainActivity : AppCompatActivity() {
         .build()
 
     private val api = retrofit.create(ChuckNorrisAPI::class.java)
-    
+    lateinit var binding: ActivityMainBinding
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.NorrisButton.setOnClickListener{
+            CoroutineScope(Dispatchers.Main).launch {
+                val responde = api.getRandomJoke()
+                binding.textViewHelloWorld.text = responde.value
+            }
+        }
     }
 }
